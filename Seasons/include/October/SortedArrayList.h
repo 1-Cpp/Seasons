abdef9648f25b8602d6a30d2b1866599e801e8d3
Martin Brunn <Martin.Brunn@arcor.de>
2015-08-09 21:44:19 +0200

    gcc problems fixed Version 0.1.1.8

--- Seasons/include/October/SortedArrayList.h
+++ Seasons/include/October/SortedArrayList.h
@@ -8,13 +8,17 @@ namespace Seasons
 	template<class TYPE,typename COMPARATOR=less<TYPE>> class SortedArrayList : public BaseArrayList<TYPE>
 	{
 	public:
+		using BaseArrayList<TYPE>::theArray;
+		using BaseArrayList<TYPE>::sizeAllocated;
+		using BaseArrayList<TYPE>::sizeChunk;
+		using BaseArrayList<TYPE>::length;
 		COMPARATOR comparator;
 		void add(const BaseArrayList<TYPE>& ar)
 		{
-			reserve(length + ar.length);
+			BaseArrayList<TYPE>::reserve(length + ar.length);
 			for (int  j = 0; j < ar.length; j++)
 			{
-				insert(ar.theArray[j]);
+				insert(ar.get(j));
 			}
 		}
 
@@ -29,7 +33,7 @@ namespace Seasons
 					break;
 				}
 			}
-			reserve(length + 2);
+			BaseArrayList<TYPE>::reserve(length + 2);
 			for (int i = length; i > index; --i)
 			{
 				theArray[i] = theArray[i - 1];
@@ -40,7 +44,7 @@ namespace Seasons
 		}
 		SortedArrayList<TYPE>& operator=(const BaseArrayList<TYPE>&otherArray)
 		{
-			clear();
+			BaseArrayList<TYPE>::clear();
 			add(otherArray);
 			return *this;
 		}
